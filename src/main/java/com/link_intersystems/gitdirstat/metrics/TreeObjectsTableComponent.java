1adc1f7ee0ee8f0f8a966dec8b70c19dea18c333
René Link <rene.link@link-intersystems.com>
2014-12-24 11:07:48 +0100

    Code clean up

--- src/main/java/com/link_intersystems/gitdirstat/metrics/TreeObjectsTableComponent.java
+++ src/main/java/com/link_intersystems/gitdirstat/metrics/TreeObjectsTableComponent.java
@@ -25,65 +25,76 @@ import com.link_intersystems.swing.TableColumnResize;
 
 public class TreeObjectsTableComponent extends GitRepositoryComponent {
 
+	private static final int COLUMN_SIZE = 1;
+
+	private static final int COLUMN_PATH = 0;
+
 	private static final long serialVersionUID = 8588810751988085851L;
 
-	private TreeObjectTableModel treeObjectsTableModel = new TreeObjectTableModel();
+	private JPanel northPanel = new JPanel(new BorderLayout());
 
-	private JTable treeObjectsTable = new JTable(treeObjectsTableModel);
 	private JTable summaryTable = new JTable();
+	private TreeObjectTableModel treeObjectsModel = new TreeObjectTableModel();
+	private TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>();
+	private TableCellRenderer defaultRenderer = new AlternatingColorTableCellRenderer();
+	private TableCellRenderer humanReadableFileSizeCellRenderer = new HumanReadableFileSizeTableCellRenderer();
+	private TableCellRenderer alternatingColorHumanReadableCellRenderer = new AlternatingColorTableCellRenderer(
+			humanReadableFileSizeCellRenderer);
+	private ComponentResize<TableColumn> tableColumnResize = new TableColumnResize();
+	private RelativeWidthResizer<TableColumn> relativeColumnWidthResize = new RelativeWidthResizer<TableColumn>(
+			tableColumnResize);
+
+	private JTable treeObjectsTable = new JTable(treeObjectsModel);
+
 	private JScrollPane treeObjectsScrollPane = new JScrollPane(
 			treeObjectsTable);
 
 	private RowSorterAwareListSelectionModelUpdater rowSorterAwareListSelectionModelUpdater;
 
 	public TreeObjectsTableComponent() {
+		configureThis();
+		configureTreeObjectsTable();
+		configureSummaryTable();
+	}
+
+	private void configureThis() {
 		setLayout(new BorderLayout());
-
-		JPanel northPanel = new JPanel();
-		northPanel.setLayout(new BorderLayout());
-
 		add(northPanel, BorderLayout.NORTH);
 		add(treeObjectsScrollPane, BorderLayout.CENTER);
-		add(summaryTable, BorderLayout.SOUTH);
+	}
 
-		treeObjectsTable.setRowSorter(new TableRowSorter<TableModel>(
-				treeObjectsTableModel));
+	private void configureTreeObjectsTable() {
+		rowSorter.setModel(treeObjectsModel);
+		treeObjectsTable.setRowSorter(rowSorter);
 
-		TableCellRenderer defaultRenderer = new AlternatingColorTableCellRenderer();
 		treeObjectsTable.setDefaultRenderer(String.class, defaultRenderer);
-
-		AlternatingColorTableCellRenderer alternatingColorTableCellRenderer = new AlternatingColorTableCellRenderer(
-				new HumanReadableFileSizeTableCellRenderer());
-
 		treeObjectsTable.setDefaultRenderer(BigInteger.class,
-				alternatingColorTableCellRenderer);
+				alternatingColorHumanReadableCellRenderer);
 
-		treeObjectsTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		treeObjectsTable.addComponentListener(relativeColumnWidthResize);
+		configureTreeObjectsTableColumns();
+	}
 
-		summaryTable.setColumnModel(treeObjectsTable.getColumnModel());
+	private void configureTreeObjectsTableColumns() {
+		TableColumnModel columnModel = treeObjectsTable.getColumnModel();
 
-		TableModel summaryTableModel = treeObjectsTableModel
-				.getSummaryTableModel();
+		TableColumn pathColumn = columnModel.getColumn(COLUMN_PATH);
+		pathColumn.setHeaderValue(treeObjectsModel.getColumnName(COLUMN_PATH));
+		relativeColumnWidthResize.setRelativeWidth(pathColumn, 0.9);
+
+		TableColumn sizeColumn = columnModel.getColumn(1);
+		sizeColumn.setHeaderValue(treeObjectsModel.getColumnName(COLUMN_SIZE));
+		sizeColumn.setMinWidth(50);
+		relativeColumnWidthResize.setRelativeWidth(sizeColumn, 0.1);
+	}
+
+	private void configureSummaryTable() {
+		TableModel summaryTableModel = treeObjectsModel.getSummaryTableModel();
 		summaryTable.setModel(summaryTableModel);
 		summaryTable.setDefaultRenderer(BigInteger.class,
 				new HumanReadableFileSizeTableCellRenderer());
-
-		TableColumnModel columnModel = treeObjectsTable.getColumnModel();
-		TableColumn firstColumn = columnModel.getColumn(0);
-		firstColumn.setHeaderValue(treeObjectsTableModel.getColumnName(0));
-
-		TableColumn secondColumn = columnModel.getColumn(1);
-		secondColumn.setHeaderValue(treeObjectsTableModel.getColumnName(1));
-
-		ComponentResize<TableColumn> columnResize = new TableColumnResize();
-		RelativeWidthResizer<TableColumn> relativeWidthResizer = new RelativeWidthResizer<TableColumn>(
-				columnResize);
-		relativeWidthResizer.setRelativeWidth(firstColumn, 0.9);
-		relativeWidthResizer.setRelativeWidth(secondColumn, 0.1);
-		secondColumn.setMinWidth(50);
-
-		relativeWidthResizer.apply(treeObjectsTable);
-		treeObjectsTable.addComponentListener(relativeWidthResizer);
+		summaryTable.setColumnModel(treeObjectsTable.getColumnModel());
+		add(summaryTable, BorderLayout.SOUTH);
 	}
 
 	protected void updateCommitRangeTree() {
@@ -91,7 +102,7 @@ public class TreeObjectsTableComponent extends GitRepositoryComponent {
 		if (gitRepositoryModel != null) {
 			PathModel pathModel = gitRepositoryModel.getPathModel();
 			ListModel entryModel = pathModel.getListModel();
-			treeObjectsTableModel.setEntryModel(entryModel);
+			treeObjectsModel.setEntryModel(entryModel);
 			if (rowSorterAwareListSelectionModelUpdater != null) {
 				rowSorterAwareListSelectionModelUpdater.setSourceModel(null,
 						null);
