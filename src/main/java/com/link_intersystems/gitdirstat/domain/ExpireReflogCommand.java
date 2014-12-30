145696dba67db1c6c012bf8cd124a9f619a8bb3e
René Link <rene.link@link-intersystems.com>
2014-12-30 14:13:20 +0100

    A ReflogEntryFilter can now be set to an ExpireReflogCommand

--- src/main/java/com/link_intersystems/gitdirstat/domain/ExpireReflogCommand.java
+++ src/main/java/com/link_intersystems/gitdirstat/domain/ExpireReflogCommand.java
@@ -1,20 +1,32 @@
 package com.link_intersystems.gitdirstat.domain;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
 
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.AllPredicate;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
 import org.eclipse.jgit.api.GitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.ReflogEntry;
 
+import com.link_intersystems.lang.Assert;
+
 public class ExpireReflogCommand extends GitCommand<Void> {
 
-	private Date expireDate;
 	private GitRepository gitRepository;
+	private ReflogEntryFilter reflogEntryFilter = NullReflogEntryFilter.INSTANCE;
+	private Predicate<ReflogEntry> expireDatePredicate = FalsePredicate
+			.falsePredicate();
 
 	protected ExpireReflogCommand(GitRepository gitRepository) {
 		super(gitRepository.getRepository());
@@ -22,13 +34,48 @@ public class ExpireReflogCommand extends GitCommand<Void> {
 		setCallable(true);
 	}
 
-	public void setExpire(Date expireDate) {
-		Date now = new Date();
-		if (now.before(expireDate)) {
-			throw new IllegalArgumentException(
-					"expireDate must be in the past or null");
+	public void setExpire(Date expireDate, TimeZone timeZone) {
+		Assert.notNull("expireDate", expireDate);
+		Assert.notNull("timeZone", timeZone);
+
+		if (expireDate == null) {
+			setExpire((Calendar) null);
+		} else {
+			Calendar expireCalendar = Calendar.getInstance(timeZone);
+			expireCalendar.setTime(expireDate);
+
+			setExpire(expireCalendar);
 		}
-		this.expireDate = expireDate;
+	}
+
+	public void setExpire(Calendar expireCalendar) {
+		if (expireCalendar == null) {
+			this.expireDatePredicate = TruePredicate.truePredicate();
+		} else {
+			Calendar nowCalendar = Calendar.getInstance();
+
+			if (expireCalendar.after(nowCalendar)) {
+				String message = MessageFormat
+						.format("expire date {0,date,full} in time zone {1} must be in the"
+								+ " past according to the current date {2,date,full} in"
+								+ " time zone {3} or null",
+								expireCalendar.getTime(),
+								expireCalendar.getTimeZone(),
+								nowCalendar.getTime(),
+								nowCalendar.getTimeZone());
+
+				throw new IllegalArgumentException(message);
+			}
+			this.expireDatePredicate = new ExpireDateFilterPredicate(
+					expireCalendar);
+		}
+	}
+
+	public void setReflogEntryFilter(ReflogEntryFilter reflogEntryFilter) {
+		if (reflogEntryFilter == null) {
+			reflogEntryFilter = NullReflogEntryFilter.INSTANCE;
+		}
+		this.reflogEntryFilter = reflogEntryFilter;
 	}
 
 	@Override
@@ -37,12 +84,29 @@ public class ExpireReflogCommand extends GitCommand<Void> {
 
 		List<Ref> refs = gitRepository.getRefs(Ref.class);
 
+		List<Predicate<ReflogEntry>> refLogEntryFilterPredicates = new ArrayList<Predicate<ReflogEntry>>();
+
+		refLogEntryFilterPredicates.add(expireDatePredicate);
+		refLogEntryFilterPredicates.add(new ReflogEntryFilterPredicateAdapter(
+				reflogEntryFilter));
+
+		Predicate<ReflogEntry> refLogEntriesFilter = AllPredicate
+				.allPredicate(refLogEntryFilterPredicates);
+
 		try {
 			for (Ref ref : refs) {
 				List<ReflogEntry> reflogEntries = ref.getReflogEntries();
-				List<ReflogEntry> nonExpiredEntries = filterExpiredReflogEntries(reflogEntries);
+
+				Iterator<ReflogEntry> reflogEntryIterator = reflogEntries
+						.iterator();
+				Iterator<ReflogEntry> filteredReflogEntryIterator = IteratorUtils
+						.filteredIterator(reflogEntryIterator,
+								refLogEntriesFilter);
+				List<ReflogEntry> nonFilteredReflogEntries = IteratorUtils
+						.toList(filteredReflogEntryIterator);
+
 				ref.clearReflog();
-				ref.addReflogEntries(nonExpiredEntries);
+				ref.addReflogEntries(nonFilteredReflogEntries);
 			}
 		} catch (IOException e) {
 			throw new GitAPIException("Unable to read reflog entries", e) {
@@ -54,31 +118,59 @@ public class ExpireReflogCommand extends GitCommand<Void> {
 		return null;
 	}
 
-	private List<ReflogEntry> filterExpiredReflogEntries(
-			List<ReflogEntry> reflogEntries) {
-		List<ReflogEntry> nonExpired = new ArrayList<ReflogEntry>();
+	public static interface ReflogEntryFilter {
 
-		for (ReflogEntry reflogEntry : reflogEntries) {
-			PersonIdent who = reflogEntry.getWho();
-
-			if (!isExpired(who)) {
-				nonExpired.add(reflogEntry);
-			}
-
-		}
-		return nonExpired;
+		public boolean accept(ReflogEntry reflogEntry);
 	}
 
-	@SuppressWarnings("unused")
-	private boolean isExpired(PersonIdent who) {
-		if (expireDate == null) {
+	private static class NullReflogEntryFilter implements ReflogEntryFilter {
+
+		private static final NullReflogEntryFilter INSTANCE = new NullReflogEntryFilter();
+
+		@Override
+		public boolean accept(ReflogEntry reflogEntry) {
 			return true;
 		}
 
-		Date when = who.getWhen();
-		TimeZone timeZone = who.getTimeZone();
-
-		return when.before(expireDate);
 	}
 
+	private static class ReflogEntryFilterPredicateAdapter implements
+			Predicate<ReflogEntry> {
+
+		private ReflogEntryFilter reflogEntryFilter;
+
+		public ReflogEntryFilterPredicateAdapter(
+				ReflogEntryFilter reflogEntryFilter) {
+			this.reflogEntryFilter = reflogEntryFilter;
+		}
+
+		@Override
+		public boolean evaluate(ReflogEntry reflogEntry) {
+			return reflogEntryFilter.accept(reflogEntry);
+		}
+	}
+
+	private static class ExpireDateFilterPredicate implements
+			Predicate<ReflogEntry> {
+
+		private Calendar expireCalendar;
+
+		public ExpireDateFilterPredicate(Calendar expireCalendar) {
+			this.expireCalendar = expireCalendar;
+		}
+
+		@Override
+		public boolean evaluate(ReflogEntry reflogEntry) {
+			PersonIdent who = reflogEntry.getWho();
+
+			TimeZone timeZone = who.getTimeZone();
+			Calendar reflogEntryCalendar = Calendar.getInstance(timeZone);
+			Date when = who.getWhen();
+			reflogEntryCalendar.setTime(when);
+
+			boolean isNotExpired = reflogEntryCalendar.after(expireCalendar);
+			return isNotExpired;
+		}
+
+	}
 }
